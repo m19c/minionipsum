19a07a32dd4c44ae1f45c1684696e2efff9ead1c
Nils Hitze <nils.hitze@boerse-go.de>
2013-10-23 16:04:27 +0200

    fixed broken Class

--- MinionIpsum.py
+++ MinionIpsum.py
@@ -1,8 +1,9 @@
-import sublime, sublime_plugin, random
+import sublime, sublime_plugin
 
-class MinionIpsum():
-	
-	def run(self, edit):
+class MinionIpsum(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+
 		bananaArray = ["pepete","aaaaaah","uuuhhh","jiji","hahaha","jeje","wiiiii","bananaaaa","bappleees","potatoooo","para tu","la bodaaa","poulet tikka masala","daa","hana dul sae","belloo!","poopayee","tank yuuu!","me want bananaaa!","underweaaar","bee do bee do bee do","tulaliloo","ti aamoo!","tatata bala tu","baboiii","po kass","gelatooo","butt","chasy"]
 		latinArray = ["sit amet", "consectetur", "adipisicing", "elit", "sed", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua", "enim", "ad", "minim", "veniam", "quis", "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "commodo", "consequat", "duis", "aute", "irure", "dolor", "reprehenderit", "voluptate", "velit", "esse", "cillum","occaecat", "qui", "officia"]
 
