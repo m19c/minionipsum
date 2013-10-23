b00699b41759fe95c0b95ee25302581e4c80bbcc
Nils Hitze <nils.hitze@boerse-go.de>
2013-10-23 16:47:31 +0200

    more madness

--- MinionIpsum.py
+++ MinionIpsum.py
@@ -1,44 +1,65 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, random, re
 
 class MinionIpsum(sublime_plugin.TextCommand):
 
-    def run(self, edit):
+    def run(self, edit, paragraphNumber = 1):
 
-		bananaArray = ["pepete","aaaaaah","uuuhhh","jiji","hahaha","jeje","wiiiii","bananaaaa","bappleees","potatoooo","para tu","la bodaaa","poulet tikka masala","daa","hana dul sae","belloo!","poopayee","tank yuuu!","me want bananaaa!","underweaaar","bee do bee do bee do","tulaliloo","ti aamoo!","tatata bala tu","baboiii","po kass","gelatooo","butt","chasy"]
-		latinArray = ["sit amet", "consectetur", "adipisicing", "elit", "sed", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua", "enim", "ad", "minim", "veniam", "quis", "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "commodo", "consequat", "duis", "aute", "irure", "dolor", "reprehenderit", "voluptate", "velit", "esse", "cillum","occaecat", "qui", "officia"]
+        bananaArray = ["pepete","aaaaaah","uuuhhh","jiji","hahaha","jeje","wiiiii","bananaaaa","bappleees","potatoooo","para tu","la bodaaa","poulet tikka masala","daa","hana dul sae","belloo!","poopayee","tank yuuu!","me want bananaaa!","underweaaar","bee do bee do bee do","tulaliloo","ti aamoo!","tatata bala tu","baboiii","po kass","gelatooo","butt","chasy"]
 
-		text = self.generateParagraph(bananaArray)
-		self.view.insert(edit, self.view.sel()[0].begin(), text)
+        for region in view.sel():  
+            if not region.empty():  
+            
+                last = re.search("(\d+)", region).group(0)
 
-	def generateParagraph(self, wordsArray):
-		oneParagraph = ""
-		linesNumber = random.randrange(5,10)
-		oneParagraph = self.generateLine(wordsArray)
+                print "will repeat " + last + " times"
+                text = self.generateFullText(last, bananaArray)
+            else: 
+                text = self.generateFullText(paragraphNumber, bananaArray)
 
-		for num in range(1,linesNumber):
-			oneParagraph = oneParagraph + self.generateLine(wordsArray)   
+        self.view.insert(edit, self.view.sel()[0].begin(), text)
 
-		oneParagraph='<p>'+oneParagraph+'</p>';
+    def generateFullText(self, paragraphNumber, wordsArray):
 
-		return oneParagraph
+        FullText = ""
+        FullText = self.generateParagraph(wordsArray)   
 
-	def generateLine(self, wordsArray):
-		oneLine = ""
-		wordsNumber = random.randrange(5,10)
-		wordsArrayLength = len(wordsArray)
-		wordRandomIndex = random.randrange(5, wordsArrayLength);
-		firstLine = True
+        for num in range(1, paragraphNumber):
+            FullText=FullText+self.generateParagraph(wordsArray);  
+    
+        return FullText
 
-		if(firstLine):
-			oneLine = "Minions ipsum"
-			firstLine=False
-		else:
-			oneLine=wordsArray[wordRandomIndex];
-		
-		for num in range(0, wordsArrayLength):
-			wordRandomIndex = random.randrange(num, len(wordsArray));		
-			wordRandom = wordsArray[wordRandomIndex]
+    def generateParagraph(self, wordsArray):
 
-			oneLine=oneLine + ' ' + wordRandom;   
+        oneParagraph = ""
+        linesNumber = random.randrange(5,10)
+        oneParagraph = self.generateLine(wordsArray)
 
-		return oneLine
+        for num in range(1, linesNumber):
+            oneParagraph = oneParagraph + self.generateLine(wordsArray)   
+
+        return oneParagraph+"\n\n"
+
+    def generateLine(self, wordsArray):
+
+        oneLine = ""
+        wordsNumber = random.randrange(5,10)
+        wordsArrayLength = len(wordsArray)
+        wordRandomIndex = random.randrange(5, wordsArrayLength);
+        firstLine = True
+
+        if(firstLine):
+            oneLine = "Minions ipsum"
+            firstLine=False
+        else:
+            oneLine=wordsArray[wordRandomIndex];
+        
+        for num in range(0, wordsArrayLength):
+            wordRandomIndex = random.randrange(num, len(wordsArray));       
+            wordRandom = wordsArray[wordRandomIndex]
+
+            oneLine=oneLine + ' ' + wordRandom;   
+
+        return oneLine
+
+
+MinionIpsum().run()
