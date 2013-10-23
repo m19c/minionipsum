61a4899696fcdf349021d08e490a090c695bd0b7
Nils Hitze <nils.hitze@boerse-go.de>
2013-10-23 15:50:45 +0200

    refactored

--- MinionIpsum.py
+++ MinionIpsum.py
@@ -1,45 +1,43 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, random
 
-class MinionIpsum(sublime_plugin.TextCommand):
+class MinionIpsum():
+	
 	def run(self, edit):
-		bananaArray = array("pepete","aaaaaah","uuuhhh","jiji","hahaha","jeje","wiiiii","bananaaaa","bappleees","potatoooo","para tu","la bodaaa","poulet tikka masala","daa","hana dul sae","belloo!","poopayee","tank yuuu!","me want bananaaa!","underweaaar","bee do bee do bee do","tulaliloo","ti aamoo!","tatata bala tu","baboiii","po kass","gelatooo","butt","chasy")
-		latinArray = array("sit amet", "consectetur", "adipisicing", "elit", "sed", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua", "enim", "ad", "minim", "veniam", "quis", "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "commodo", "consequat", "duis", "aute", "irure", "dolor", "reprehenderit", "voluptate", "velit", "esse", "cillum","occaecat", "qui", "officia")
+		bananaArray = ["pepete","aaaaaah","uuuhhh","jiji","hahaha","jeje","wiiiii","bananaaaa","bappleees","potatoooo","para tu","la bodaaa","poulet tikka masala","daa","hana dul sae","belloo!","poopayee","tank yuuu!","me want bananaaa!","underweaaar","bee do bee do bee do","tulaliloo","ti aamoo!","tatata bala tu","baboiii","po kass","gelatooo","butt","chasy"]
+		latinArray = ["sit amet", "consectetur", "adipisicing", "elit", "sed", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua", "enim", "ad", "minim", "veniam", "quis", "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "commodo", "consequat", "duis", "aute", "irure", "dolor", "reprehenderit", "voluptate", "velit", "esse", "cillum","occaecat", "qui", "officia"]
 
 		text = self.generateParagraph(bananaArray)
 		self.view.insert(edit, self.view.sel()[0].begin(), text)
 
-		def generateParagraph(self, wordsArray):
-			oneParagraph = ""
-			linesNumber = 5 + (10-5) * self.random()
+	def generateParagraph(self, wordsArray):
+		oneParagraph = ""
+		linesNumber = random.randrange(5,10)
+		oneParagraph = self.generateLine(wordsArray)
 
-			oneParagraph = self.generateLine(wordsArray)
+		for num in range(1,linesNumber):
+			oneParagraph = oneParagraph + self.generateLine(wordsArray)   
 
-			for num in range(1,linesNumber):
-				oneParagraph = oneParagraph + generateLine(wordsArray)   
+		oneParagraph='<p>'+oneParagraph+'</p>';
 
-			oneParagraph='<p>'+oneParagraph+'</p>';
+		return oneParagraph
 
-			return oneParagraph
+	def generateLine(self, wordsArray):
+		oneLine = ""
+		wordsNumber = random.randrange(5,10)
+		wordsArrayLength = len(wordsArray)
+		wordRandomIndex = random.randrange(5, wordsArrayLength);
+		firstLine = True
 
-		def generateLine(self, wordsArray):
-			oneLine = ""
-			wordsNumber = 5 + (10-5) * self.random()
-			wordRandomIndex = 5 + (len(wordsArray)-5) * self.random();
-	
-			if(firstLine):
-				oneLine = "Minions ipsum"
-				firstLine=false
-			else:
-				oneLine=wordsArray[wordRandomIndex];
-			
-			for num in range(0, len(wordsArray)):
-				wordRandomIndex = 5 + (len(wordsArray)-5) * self.random();		
-				wordRandom = wordsArray[wordRandomIndex]
+		if(firstLine):
+			oneLine = "Minions ipsum"
+			firstLine=False
+		else:
+			oneLine=wordsArray[wordRandomIndex];
+		
+		for num in range(0, wordsArrayLength):
+			wordRandomIndex = random.randrange(num, len(wordsArray));		
+			wordRandom = wordsArray[wordRandomIndex]
 
-				oneLine=oneLine + ' ' + wordRandom;   
-	
+			oneLine=oneLine + ' ' + wordRandom;   
 
-			return oneLine
-
-	
-
+		return oneLine
